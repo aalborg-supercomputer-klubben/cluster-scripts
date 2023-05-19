1836b60998b2cdc9124c4207501300c23f9952a5
Anders Aaen Springborg <aaspringborg@gmail.com>
2023-05-20 00:26:48 +0200

    poweroff

---
+++ poweroff-all.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ssh 192.168.0.101 systemctl poweroff &
+ssh 192.168.0.102 systemctl poweroff &
+ssh 192.168.0.103 systemctl poweroff &
+ssh 192.168.0.104 systemctl poweroff &
+ssh 192.168.0.105 systemctl poweroff &
+ssh 192.168.0.106 systemctl poweroff &
+ssh 192.168.0.107 systemctl poweroff &
+ssh 192.168.0.108 systemctl poweroff &
+ssh 192.168.0.109 systemctl poweroff &
+ssh 192.168.0.110 systemctl poweroff &
+ssh 192.168.0.111 systemctl poweroff &
+ssh 192.168.0.112 systemctl poweroff &
+ssh 192.168.0.113 systemctl poweroff &
+ssh 192.168.0.114 systemctl poweroff &
+ssh 192.168.0.115 systemctl poweroff &
+ssh 192.168.0.116 systemctl poweroff &
